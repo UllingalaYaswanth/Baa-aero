1b66735b2759c003dbc33a7f2301fbbe50841b84
yaswanth ullingala <yaswanthullingala@gmail.com>
2025-07-23 20:29:06 +0530

    updated new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+// import 'animate.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css'
+// import { BrowserRouter as Router } from 'react-router-dom'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>
+// )
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'animate.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
