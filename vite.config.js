9b6f043f52e48cb6f5d7cb29eb6a3234f94fa195
yaswanth ullingala <yaswanthullingala@gmail.com>
2025-07-23 20:40:24 +0530

    updated new changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Baa-aero/',  // 👈 Add this line (exactly matches your GitHub Pages repo name)
+  server: {
+    allowedHosts: ['baa-aero.onrender.com']
+  }
 })
