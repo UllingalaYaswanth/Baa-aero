07a00a24d7be47b295aa9c5f4a0eb01a03679a2c
yaswanth <yaswanthullingala@gmail.com>
2025-08-05 15:50:23 +0530

    connected to cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/Baa-aero/',  // 👈 Add this line (exactly matches your GitHub Pages repo name)
-  server: {
-    allowedHosts: ['baa-aero.onrender.com']
-  }
 })
